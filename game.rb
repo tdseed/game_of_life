2a681a6e0a5901e19a1d75987cfbbe10001f6500
tdseed <tdseed1219@gmail.com>
2013-04-06 09:24:37 +0800

    most of all

--- game.rb
+++ game.rb
@@ -2,15 +2,87 @@ class Game
 
   def initialize(grid)
   	@grid = grid
-  	@grid_length = grid.length
-  end
 
-  def init_grid
-    # Array.new(@grid_length) { [0] }
-    @grid_length.times.map { [0] }
+    init_length
+  	init_result_grid
   end
 
   def tick
-  	init_grid
+  	@y_length.times do |i|
+   	  if alive?(i,0) && has_2_alive_neighbors?(i, 0)
+  	  set_alive(i,0)
+      end
+    end
+    
+    @x_length.times do |j|
+      if alive?(0,j) && has_2_alive_x_neighbors?(j)
+        set_alive(0,j)
+      end
+    end
+  
+    if @x_length > 1 && @y_length > 1 &&
+      alive?(0,0) &&
+      right_cell_alive?(0,0) && 
+      down_cell_alive?(0,0)
+
+      set_alive(0,0)
+    end
+
+    if @x_length > 1 && @y_length > 1 &&
+      alive?(1,1) &&
+      left_cell_alive?(1,1) && 
+      up_cell_alive?(1,1)
+
+      set_alive(1,1)
+    end
+
+  	@next_tick_grid
   end
+
+  private
+
+  def init_result_grid 
+      @next_tick_grid = @y_length.times.map { [0]*@x_length}
+  end
+
+  def init_length
+    @y_length = @grid.length
+    @x_length = @grid[0].length
+  end
+
+  def alive?(i, j)
+    @grid[i][j] == 1
+  end
+
+  def set_alive(i, j)
+    @next_tick_grid[i][j] = 1
+  end
+
+  def has_2_alive_x_neighbors?(j) 
+    count = 0
+    count += 1 if left_cell_alive?(0, j)
+    count += 1 if right_cell_alive?(0, j)
+    count == 2
+  end
+
+  def has_2_alive_neighbors?(i, j)
+    up_cell_alive?(i, j) && down_cell_alive?(i, j)
+  end
+
+  def up_cell_alive?(i, j)
+    i > 0 && alive?(i-1, j)
+  end
+
+  def down_cell_alive?(i, j)
+    i < @y_length-1 && alive?(i+1, j)
+  end
+
+  def left_cell_alive?(i, j)
+    j > 0 && alive?(i, j-1)
+  end
+
+  def right_cell_alive?(i, j)
+    j < @x_length && alive?(i, j+1)
+  end
+  
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,38 +1,143 @@
 require 'spec_helper'
 
-def assert_grid(init_grid,expected_grid)
+def match_grid(init_grid,expected_grid)
   game = Game.new init_grid
   game.tick.should == expected_grid
 end
 
 describe Game do
   
-  context "only one cell" do
-  	it "one cell will still be dead" do
-  	  assert_grid [[0]],[[0]]
-  	  assert_grid [[1]],[[0]]
+  context "asix y" do
+ 
+  	it "1 cell" do
+  	  match_grid [[0]],[[0]]
+
+  	  match_grid [[1]],[[0]]
   	end
-  end
+  
 
+  	it "2 cells" do
+  	  match_grid [[0],[0]],
+  	             [[0],[0]]	
 
-  context "two cells" do
-  	it "two cells will still be dead" do
-  	  assert_grid [[0],[0]],
-  	              [[0],[0]]	
-  	  assert_grid [[1],[0]],
-  	              [[0],[0]]
-      assert_grid [[0],[1]],
-      			  [[0],[0]]
-  	  assert_grid [[1],[1]],
-  	  			  [[0],[0]]      			  
+  	  match_grid [[1],[0]],
+  	             [[0],[0]]
+
+      match_grid [[0],[1]],
+      			     [[0],[0]]
+
+  	  match_grid [[1],[1]],
+  	  			     [[0],[0]]      			  
   	end
-  end
 
-  context "three cells" do
-    it "three dead cells will die" do
-      assert_grid [[0],[0],[0]],
-                  [[0],[0],[0]]
+    context "3 cells" do
+      it "all dead" do
+        match_grid [[0],[0],[0]],
+                   [[0],[0],[0]]
+
+        match_grid [[1],[0],[0]],
+                   [[0],[0],[0]]
+
+        match_grid [[1],[1],[0]],
+                   [[0],[0],[0]]
+      end
+
+      it "keep alive" do
+        match_grid [[1],[1],[1]],
+                   [[0],[1],[0]]
+      end
     end	
+
+    it ">=4 cells" do
+      match_grid [[0],[0],[0],[0]],
+                 [[0],[0],[0],[0]]  
+
+      match_grid [[1],[0],[0],[0]],
+                 [[0],[0],[0],[0]]
+
+      match_grid [[1],[1],[0],[0]],
+                 [[0],[0],[0],[0]] 
+
+      match_grid [[1],[1],[1],[0]],
+                 [[0],[1],[0],[0]] 
+
+      match_grid [[0],[1],[1],[1]],
+                 [[0],[0],[1],[0]]  
+
+      match_grid [[1],[1],[1],[1]],
+                 [[0],[1],[1],[0]]
+
+      match_grid [[1],[1],[1],[1],[1]],
+                 [[0],[1],[1],[1],[0]]       
+    end
+
   end
 
+  context "asix Y" do
+    it "2 cells" do
+      match_grid [[0,0]],
+                 [[0,0]]
+
+      match_grid [[1,0]],
+                 [[0,0]]
+
+      match_grid [[1,1]],
+                 [[0,0]]
+    end
+
+    context "3 cells" do
+      it "all dead" do
+        match_grid [[0,0,0]],
+                   [[0,0,0]]
+
+        match_grid [[1,0,0]],
+                   [[0,0,0]]
+
+        match_grid [[1,1,0]],
+                   [[0,0,0]]
+      end
+
+      it "keep alive" do
+        match_grid [[1,1,1]],
+                   [[0,1,0]]
+      end               
+    end
+
+    it "4 cells" do
+      match_grid [[0,0,0,0]],
+                 [[0,0,0,0]]  
+
+      match_grid [[1,0,0,0]],
+                 [[0,0,0,0]]  
+
+      match_grid [[1,1,0,0]],
+                 [[0,0,0,0]]  
+
+      match_grid [[1,1,1,0]],
+                 [[0,1,0,0]]  
+
+      match_grid [[0,1,1,1]],
+                 [[0,0,1,0]]
+
+      match_grid [[1,1,1,1]],
+                [[0,1,1,0]]
+    end
+  end
+  
+  context "2 asix" do
+    it "2X2 cells" do
+      match_grid [[0,0],[0,0]],
+                 [[0,0],[0,0]]
+
+      match_grid [[1,1],[0,0]],
+                 [[0,0],[0,0]]
+
+      match_grid [[1,1],[1,0]],
+                 [[1,0],[0,0]]
+
+      match_grid [[0,1],[1,1]],
+                 [[0,0],[0,1]]
+
+    end
+  end
 end
